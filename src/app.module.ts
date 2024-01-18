55716d9dacb2cfa827bed835dc8c5b8c80cd0748
ogabek <jurayev.ogabek0523@gmail.com>
2024-01-18 13:05:43 +0500

    fixed typeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './module/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectDb } from './config/typeorm'
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot(connectDb)],
 })
 export class AppModule { }
